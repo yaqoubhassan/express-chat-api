f7ec9062a877a3159d44b25d916d473fc1724eee
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-20 21:40:42 +0000

    Add logic for verifyig emails and resending otp

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,9 @@
 const User = require("../models/userModel");
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-};
+const sendEmail = require("../utils/sendEmail");
+const generateOtp = require("../utils/generateOtp");
+const generateToken = require("../utils/generateToken");
+const findUserByEmail = require("../middlewares/findUser");
+const handleRequestAndServerErrors = require("../utils/errorHandler");
 
 const registerUser = async (req, res) => {
   const { name, email, password, passwordConfirmation } = req.body;
@@ -17,14 +15,35 @@ const registerUser = async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({
-        status: "error",
-        message: "User already exists",
-      });
+      return handleRequestAndServerErrors(
+        (error = null),
+        res,
+        400,
+        "error",
+        "User already exists"
+      );
+      // return res.status(400).json({
+      //   status: "error",
+      //   message: "User already exists",
+      // });
     }
 
     const user = new User({ name, email, password });
+
+    const { otp, otpExpires } = generateOtp();
+    user.otp = otp;
+    user.otpExpires = otpExpires;
+
     await user.save();
+
+    const emailHtml = `
+      <h1>Email Verification</h1>
+      <p>Your verification code is:</p>
+      <h2>${otp}</h2>
+      <p>This code will expire in 10 minutes</p>
+    `;
+
+    await sendEmail(email, "Verify Your Email", emailHtml);
 
     res.status(201).json({
       status: "success",
@@ -37,6 +56,87 @@ const registerUser = async (req, res) => {
       },
     });
   } catch (error) {
+    handleRequestAndServerErrors(error, res, 500, "fail", "Server Error");
+  }
+};
+
+const verifyEmail = async (req, res) => {
+  const { email, otp } = req.body;
+
+  try {
+    const user = await findUserByEmail(email);
+
+    if (user.verified) {
+      return res.status(400).json({
+        status: "error",
+        message: "User is already verified.",
+      });
+    }
+
+    if (user.otp !== otp) {
+      return res.status(400).json({
+        status: "error",
+        message: "Invalid verification code.",
+      });
+    }
+
+    if (user.otpExpires < Date.now()) {
+      return res.status(400).json({
+        status: "error",
+        message: "Verification code has expired",
+      });
+    }
+
+    user.verified = true;
+    user.otp = null;
+    user.otpExpires = null;
+    await user.save();
+
+    res.status(200).json({
+      status: "success",
+      message: "Email verified successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "failed",
+      message: "Server Error",
+    });
+  }
+};
+
+const resendOtp = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await findUserByEmail(email);
+
+    if (user.verified) {
+      return res.status(400).json({
+        status: "error",
+        message: "User is already verified",
+      });
+    }
+
+    const { otp, otpExpires } = generateOtp();
+    user.otp = otp;
+    user.otpExpires = otpExpires;
+    await user.save();
+
+    const emailHtml = `
+      <h1>Email Verification</h1>
+      <p>Your new verification code id:</p>
+      <h2>${otp}</h2>
+      <p>This code will expire in 10 minutes</p>
+    `;
+
+    await sendEmail(email, "Resend Verification Code", emailHtml);
+
+    res.status(200).json({
+      status: "success",
+      message: "Verification code resent successfully.",
+    });
+  } catch (error) {
     console.error(error);
     res.status(500).json({
       status: "failed",
@@ -45,4 +145,4 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+module.exports = { registerUser, verifyEmail, resendOtp };
