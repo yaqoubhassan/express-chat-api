2443771b483c76925480a2e57d5d25eb1c38b27d
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-26 13:02:24 +0000

    Update message model

--- models/messageModel.js
+++ models/messageModel.js
@@ -6,19 +6,42 @@ const messageSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Conversation",
       required: true,
+      index: true, // Index for faster lookup
     },
     sender: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
+    type: {
+      type: String,
+      enum: ["text", "image", "video", "audio"], // Define message types
+      default: "text",
+    },
     content: {
       type: String,
-      required: [true, "The content field is required"],
+      required: function () {
+        return this.type === "text";
+      },
+    },
+    media: {
+      type: String, // URL for image or video
+      required: function () {
+        return this.type === "image" || this.type === "video";
+      },
+    },
+    audioDuration: {
+      type: String, // Duration of the audio file
+      required: function () {
+        return this.type === "audio";
+      },
     },
     read: {
       type: Boolean,
       default: false,
+    },
+    readAt: {
+      type: Date, // Timestamp of when the message was read
     },
   },
   { timestamps: true }
