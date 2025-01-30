94b28bff7e46b073bebbfa04c552252616b70be1
Yakubu Alhassan <yaqoubdramani@gmail.com>
2025-01-30 20:49:20 +0000

    Add endpoint for marking messages as read

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { sendMessage } = require("../controllers/messageController");
+const {
+  sendMessage,
+  markMessageAsRead,
+} = require("../controllers/messageController");
 const { authMiddleware } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, sendMessage);
+router.patch("/:messageId/read", authMiddleware, markMessageAsRead);
 
 module.exports = router;
